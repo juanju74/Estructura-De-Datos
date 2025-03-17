3341f8e5a46632e588ac09ea996227c802fd0682
juanju74 <juan.arboledamo@campusucc.edu.co>
2025-03-17 16:08:35 +0000

    teamo liz

--- Tranajo_En_Clase_Sesison_7/Ejercicio1.cpp
+++ Tranajo_En_Clase_Sesison_7/Ejercicio1.cpp
@@ -1,18 +1,18 @@
-#include <iostream>
-#include <stack>
-#include "encabezado.c++"
+    #include <iostream>
+    #include <stack>
+    #include "encabezado.c++"
 
-using namespace std;
+    using namespace std;
 
-int main(){
-    imprimirencabezado();
-    stack<string> historial;
-    historial.push("www.google.com");
-    historial.push("www.github.com");
-    historial.push("www.stackoverflow.com");
+    int main(){
+        imprimirencabezado();
+        stack<string> historial;
+        historial.push("www.google.com");
+        historial.push("www.github.com");
+        historial.push("www.stackoverflow.com");
 
-    cout << "Página actual: " << historial.top() << endl;
-    historial.pop();
-    cout << "Retrocediendo... Ahora en: " << historial.top() << endl;
-    return 0;
-}
+        cout << "Página actual: " << historial.top() << endl;
+        historial.pop();
+        cout << "Retrocediendo... Ahora en: " << historial.top() << endl;
+        return 0;
+    }
